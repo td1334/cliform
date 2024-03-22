6f10d290a5be4d14c388db66b2ade93d990fa29b
Peter Alexander <td1334@scaip.de>
2024-03-22 20:44:48 +0100

    Version 0.1

--- index.php
+++ index.php
@@ -2,22 +2,21 @@
 
 namespace td1334;
 
+use td1334\cliform;
 use td1334\cliform\CliFormQuestion;
 
 include "vendor/autoload.php";
 
-$frage = (new CliFormQuestion("frage"))
-        ->setDefault("yes")
-        ->setRequirements("no,maybe")
-        ->setCaseSensitive(false)
-    ;
-
-$frage = (new CliFormQuestion("frage", "yes", "yes, no, maybe", false));
-$frage = (new CliFormQuestion("frage"))->run();
-$frage = (new CliFormQuestion("frage", "yes", "yes, no, maybe", false));
-
-
-$antwort = $frage->run();
-
+/*
+//$frage = (new CliFormQuestion("frage", "yes", "yes, no, maybe", false));
+//$antwort = $frage->run();
+$antwort = (new CliFormQuestion("frage", "yes", "yes, no, maybe", false))->run();
 //print_r($frage);
 print_r($antwort);
+*/
+
+$form = new cliform\Cliform();
+$form->add("vorname", new CliFormQuestion("Vorname", "yes", "yes, no, maybe", false));
+$form->add("strasse", new CliFormQuestion("Strasse"));
+$form->run();
+print_r($form->getValue("strasse"));
